07ce669b84093bb944e476c46d10c5e92746b879
jmaks <my.mailname@mail.ru>
2016-04-17 19:00:37 +0300

    gulp

--- gulpfile.js
+++ gulpfile.js
@@ -16,26 +16,20 @@ var task = requireDir('./gulp/tasks', { recurse: true });
 // init all tasks
 task.javascripts();
 task.spritePng();
-//task.spriteSvg();
-//task.icons();
 task.stylesheets();
-//task.watch();
+task.watch();
 
 // init all production tasks
 task.javascripts(runTimestamp);
 task.spritePng(runTimestamp);
-//task.spriteSvg(runTimestamp);
-//task.icons(runTimestamp);
 task.stylesheets(runTimestamp);
 
 gulp.task('development', function (cb) {
     gulpSequence(
         'javascripts',
         'spritePng',
-        //'spriteSvg',
-        //'icons',
-        'stylesheets'
-        //'watch'
+        'stylesheets',
+        'watch'
     )(cb);
 });
 
@@ -44,8 +38,6 @@ gulp.task('production', function (cb) {
     gulpSequence(
         'javascripts:production',
         'spritePng:production',
-        //'spriteSvg:production',
-        //'icons:production',
         'stylesheets:production'
     )(cb);
 });
